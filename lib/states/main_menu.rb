e7e0173e28df8d1c6acf0c95049c4891fb775b21
Cyberarm <matthewlikesrobots@gmail.com>
2019-12-29 11:31:04 -0600

    kinda sorta works again

--- lib/states/main_menu.rb
+++ lib/states/main_menu.rb
@@ -3,13 +3,12 @@ module RenegadeFriends
   require "json"
   class MainMenu < CyberarmEngine::GuiState
     def setup
-      @cwidth, @cheight = window.width, window.height
       @data = JSON.parse(open("http://192.168.1.111:3000/api/game-servers").read)
 
       self.show_cursor = true
       background Gosu::Color.rgb(45, 29, 19)
 
-      @header = flow width: window.width, padding_top: 20, padding_bottom: 20 do
+      @header = flow width: 1.0, padding_top: 20, padding_bottom: 20 do
         background [
           Gosu::Color::BLACK, Gosu::Color::BLACK,
           Gosu::Color.rgb(45, 29, 19), Gosu::Color.rgb(45, 29, 19)
@@ -17,6 +16,7 @@ module RenegadeFriends
 
         label "Renegade Friends", text_size: 40, color: Gosu::Color.rgb(255,127,0)
         button "Home", margin_left: 10 do
+          @content.clear { populate_server_list(@data) }
         end
         button "Friends", margin_left: 10 do
         end
@@ -25,39 +25,32 @@ module RenegadeFriends
         end
       end
 
-      @content = flow width: window.width, height: window.height, margin_left: 15 do
-        @data.reject {|s| !s["name"].include?("C&C")}.each do |server|
-          stack do
-            background Gosu::Color.rgb(rand(100), rand(100), rand(100))
-            flow do
-              label server["name"], margin_top: 8, text_shadow: true
-              button "Join", margin_left: 10
-            end
-            label "Map #{server["mapname"]} ✅"
-            label "Time left #{server["timeleft"]}"
-            label "Players #{server["numplayers"]}/#{server["maxplayers"]}"
-            label ""
-
-            server["players"].each do |player|
-              player["ping"] ||= 0
-              label "#{player["team"].downcase.include?("nod") ? "<c=ffff0000>#{player["player"]}</c>" : "<c=ffFFA500>#{player["player"]}</c>"} on #{player["team"]} score #{player["score"]} K/D #{(player["kills"].to_f/player["deaths"].to_f).round(2)}. (#{player["ping"]}ms)"
-            end
-          end
-        end
+      @content = flow width: 1.0, height: 1.0, margin_left: 15, margin_right: 15 do
+        populate_server_list(@data)
       end
     end
 
-    def update
-      super
+    def populate_server_list(data)
+      data.reject {|s| !s["name"].include?("C&C")}.each do |server|
+        stack padding: 3 do
+          background Gosu::Color.rgb(rand(100), rand(100), rand(100))
+          flow do
+            label server["name"], margin_top: 8, text_shadow: true
+            button "Join", margin_left: 10
+          end
+          label "Map #{server["mapname"]} ✅"
+          label "Time left #{server["timeleft"]}"
+          label "Players #{server["numplayers"]}/#{server["maxplayers"]}"
+          label ""
 
-      if @cwidth != window.width || @cheight != window.height
-        @cwidth,@cheight = window.width, window.height
-
-        @header.width = window.width
-        @content.width  = window.width
-        @content.height = window.height
-
-        @root_container.recalculate
+          server["players"].each_with_index do |player, i|
+            player["ping"] ||= 0
+            stack width: 1.0 do
+              background i.even? ? 0x55666666 : 0x55000000
+              label "#{player["team"].downcase.include?("nod") ? "<c=ffff0000>#{player["player"]}</c>" : "<c=ffFFA500>#{player["player"]}</c>"} ⁠score #{player["score"]} ⁠- K/D #{(player["kills"].to_f/player["deaths"].to_f).round(2)} ⁠- #{player["ping"]}ms"
+            end
+          end
+        end
       end
     end
   end
--- renegade_friends.rb
+++ renegade_friends.rb
@@ -9,10 +9,10 @@ require_relative "lib/states/main_menu"
 require_relative "lib/master_server/master_server"
 require_relative "lib/server_list/server_list"
 
-require_relative "lib/containers/header"
-require_relative "lib/containers/home"
-require_relative "lib/containers/about"
-require_relative "lib/containers/friends"
+# require_relative "lib/containers/header"
+# require_relative "lib/containers/home"
+# require_relative "lib/containers/about"
+# require_relative "lib/containers/friends"
 
 
 
@@ -21,7 +21,7 @@ module RenegadeFriends
   VALID_EXECUTABLES = ["game.exe", "game2.exe"]
   class Window < CyberarmEngine::Engine
     def initialize
-      super(640, 480, fullscreen: false, resizable: true)
+      super(width: 640, height: 480, fullscreen: false, resizable: true)
     end
     def setup
       push_state(MainMenu)
